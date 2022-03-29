cd126e84c913dbbf610b83e080cdbe2a7f6ef0aa
sameers999 <syedmahammadsameer999>
2022-03-30 00:02:06 +0530

    Snake And Lader

---
+++ src/SnakeAndLadder_1/SnakeAndLaddder1.java
@@ -0,0 +1,12 @@
+package SnakeAndLadder_1;
+
+public class SnakeAndLaddder1 {
+	
+	public static void main(String[] args) {
+		int var = 1;
+		int startPosition = 0;
+		
+		System.out.println("Player = " + var);
+	    System.out.println("Position = " + startPosition);
+	}
+}
